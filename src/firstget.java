83a26e95e06fab99825589c64bd484910a1b2035
sendres1 <sendres1@L11104042784.wctc.msft>
2014-08-25 18:59:34 -0500

    add 1

--- src/firstget.java
+++ src/firstget.java
@@ -1,13 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 /**
  *
  * @author sendres1
  */
 public class firstget {
+   
     
+    System.out.println('I');
 }
